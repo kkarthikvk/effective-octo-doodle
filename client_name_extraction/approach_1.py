2a43cc376c1db2ec2e685a41f77aa6813a1303bc
karthik <kkarthikeyanvk@gmail.com>
2020-03-02 14:54:56 +0530

    client_name_extraction/ modified

--- client_name_extraction/approach_1.py
+++ client_name_extraction/approach_1.py
@@ -50,6 +50,7 @@ def feeder(file_path, text_content):
 #    print(client_map)
     if max(list(client_map.values())) != 0:
         client = max(client_map.items(), key=operator.itemgetter(1))[0]
+	print(client)
         return [{"inference": client}]
     else:
         return [{"inference": 'Unknown'}]
